3ffac06621e13b9f6001f3fb1a287a6c37dc2ce5
Zachary J Roberts <zjroberts31@gmail.com>
2024-03-27 23:51:10 -0400

    created a query file for the db

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MySQLConsoleApp
+{
+    class Prog()
+    {
+        static void Main(string[] args){
+            Console.WriteLine("Enter UserName:");
+            String username = Console.ReadLine();
+            Console.WriteLine("Enter Password:");
+            String password = Console.ReadLine();
+            Console.WriteLine(username + " , " + password);
+
+        }
+    }
+}
+ 
